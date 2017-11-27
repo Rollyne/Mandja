2f2c57763903a0323a3bfe5150695cead6fda4f6
Dimitar Tonchev <rollyne@abv.bg>
2017-11-27 23:03:14 +0200

    Update settings.py

--- fla_net/fla_net/settings.py
+++ fla_net/fla_net/settings.py
@@ -79,16 +79,8 @@ WSGI_APPLICATION = 'fla_net.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fla_net',
-        'USER': 'juji',
-        'PASSWORD': 'bobec',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+import dj_database_url
+DATABASES = {default': dj_database_url.config()}
 
 
 # Password validation
